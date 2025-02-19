7ce8456e3d3f2973c6a637f877b8c2ccf02a19eb
Alejopizarro <alejomkt10@gmail.com>
2025-02-19 11:48:05 +0100

    formatted datas

--- src/api/category/controllers/category.js
+++ src/api/category/controllers/category.js
@@ -1,9 +1,39 @@
-'use strict';
+"use strict";
 
 /**
  *  category controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::category.category');
+module.exports = createCoreController(
+  "api::category.category",
+  ({ strapi }) => ({
+    async find(ctx) {
+      const { data, meta } = await super.find(ctx);
+
+      const formattedData = data.map((category) => ({
+        id: category.id,
+        attributes: {
+          categoryName: category.categoryName,
+          slug: category.slug,
+          createdAt: category.createdAt,
+          updatedAt: category.updatedAt,
+          publishedAt: category.publishedAt,
+          isFeatured: category.isFeatured,
+          mainImage: category.mainImage
+            ? {
+                id: category.mainImage.id,
+                attributes: {
+                  url: category.mainImage.url,
+                  formats: category.mainImage.formats || null,
+                },
+              }
+            : null,
+        },
+      }));
+
+      return { data: formattedData, meta };
+    },
+  })
+);
--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -1,9 +1,42 @@
-'use strict';
+"use strict";
 
 /**
  * product controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::product.product');
+module.exports = createCoreController("api::product.product", ({ strapi }) => ({
+  async find(ctx) {
+    // Llamamos a la implementación predeterminada de Strapi
+    const { data, meta } = await super.find(ctx);
+
+    // Modificamos la estructura de la respuesta
+    const formattedData = data.map((product) => ({
+      id: product.id,
+      attributes: {
+        productName: product.productName,
+        slug: product.slug,
+        description: product.description,
+        active: product.active,
+        price: product.price,
+        brand: product.brand,
+        size: product.size,
+        createdAt: product.createdAt,
+        updatedAt: product.updatedAt,
+        publishedAt: product.publishedAt,
+        isFeatured: product.isFeatured,
+        images: {
+          data: product.images
+            ? product.images.map((img) => ({
+                id: img.id,
+                attributes: { url: img.url },
+              }))
+            : [],
+        },
+      },
+    }));
+
+    return { data: formattedData, meta };
+  },
+}));
