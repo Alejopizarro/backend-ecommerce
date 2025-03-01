ff96a8dc249009f3add7281178d190bd4ba6436b
Alejopizarro <alejomkt10@gmail.com>
2025-03-01 13:41:52 +0100

    fix database to deploy

--- config/database.js
+++ config/database.js
@@ -1,7 +1,25 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: "postgres",
-    connection: env("DATABASE_URL", ""),
-    ssl: env.bool("DATABASE_SSL", true) ? { rejectUnauthorized: false } : false,
-  },
-});
+const parse = require("pg-connection-string").parse;
+
+module.exports = ({ env }) => {
+  const dbConfig = parse(env("DATABASE_URL"));
+
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: dbConfig.host,
+        port: dbConfig.port,
+        database: dbConfig.database,
+        user: dbConfig.user,
+        password: dbConfig.password,
+        ssl: env.bool("DATABASE_SSL", true)
+          ? { rejectUnauthorized: false }
+          : false,
+      },
+      pool: {
+        min: 2,
+        max: 10,
+      },
+    },
+  };
+};
